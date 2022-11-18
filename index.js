64d02fdb3f20644f76797b0b08b7dd041f3c9b45
rose534 <wambuirose534@gmail.com>
2022-11-18 16:05:44 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(name) {
+   cats.push("Ralph");
+   return cats; }
+
+   function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    return cats; }
+    
+    function destructivelyRemoveLastCat(name) {
+        cats.pop();
+        return cats; }
+
+    function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+    return cats; }
+
+    function appendCat(name) {
+    const newcats = [...cats, "Broom"]
+    return newcats; }
+
+function prependCat(name) {
+    const catsarray = ["Arnold", ...cats];
+    return catsarray;
+}
+    
+function removeLastCat(name) {
+const cutcat = [...cats]
+cutcat.pop()
+return cutcat; }
+
+function removeFirstCat(name) {
+const cutcat1 = [...cats]
+cutcat1.shift()
+return cutcat1;
+}
+  
